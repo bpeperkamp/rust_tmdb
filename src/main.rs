ff314122ae56fbb07db75b5018d9c520b1c9bc8b
Bart <bart@peperkamp.dev>
2024-06-04 19:10:10 +0200

    Slection possible

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use crossterm::style::Stylize;
-use serde::Deserialize;
-use reqwest::Error;
-use urlencoding::encode;
 use clap::Parser;
+#[allow(unused_imports)]
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
+#[allow(unused_imports)]
+use crossterm::style::{Print, Stylize};
+#[allow(unused_imports)]
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use dotenv::dotenv;
+use reqwest::Error;
+use serde::Deserialize;
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, Write};
+use urlencoding::encode;
+#[allow(unused_imports)]
+use dialoguer::{Select, Input, theme::ColorfulTheme};
 
-#[allow(dead_code)]
-#[derive(Deserialize)]
+// #[allow(dead_code)]
+#[derive(Deserialize, Debug)]
 struct Item {
     id: u32,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -19,6 +28,8 @@ struct Item {
     release_date: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     original_language: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    media_type: Option<String>,
 }
 
 #[derive(Parser, Debug)]
@@ -26,17 +37,17 @@ struct Item {
 #[command(version = "1.0")]
 #[command(about = "It searches for your searchterm on TMDB and returns results.")]
 struct Args {
-    /// Title of the serie or movie to lookup
-    #[arg(short, long)]
-    title: String,
+    // Title of the serie or movie to lookup
+    // #[arg(short, long)]
+    // title: String,
 
-    /// Media type, serie or movie
-    #[arg(short, long, default_value = "tv")]
-    media_type: String,
+    // Media type, serie or movie
+    // #[arg(short, long, default_value = "tv")]
+    // media_type: String,
 
-    /// Page number of the results (20 results per page)
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    // Page number of the results (20 results per page)
+    // #[arg(short, long, default_value_t = 1)]
+    // count: u8,
 }
 
 #[derive(Deserialize)]
@@ -46,20 +57,29 @@ struct Items {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-
     dotenv().ok();
 
     let bearer_token = std::env::var("TMDB_TOKEN").expect("TMDB_TOKEN must be set.");
 
-    let args = Args::parse();
+    // let args = Args::parse();
 
-    let base_url = "https://api.themoviedb.org/3/search/";
-    let search_type = if args.media_type == "movie" { "movie" } else { "tv" };
-    let page_number =  format!("?page=") +  &args.count.to_string();
-    let params = "&include_adult=false&language=en-US&page=1&query=";
-    let search_term = encode(&args.title);
+    let base_url = "https://api.themoviedb.org/3/search/multi";
 
-    let complete_url = format!("{base_url}{search_type}{page_number}{params}{search_term}");
+    // let search_type = if args.media_type == "movie" {
+    //     "movie"
+    // } else {
+    //     "tv"
+    // };
+
+    let title: String = Input::new()
+        .with_prompt("Search for")
+        .interact_text()
+        .unwrap();
+
+    let params = "?page=1&include_adult=false&language=en-US&page=1&query=";
+    let search_term = encode(&title);
+
+    let complete_url = format!("{base_url}{params}{search_term}");
 
     let response = reqwest::Client::new()
         .get(complete_url)
@@ -71,36 +91,34 @@ async fn main() -> Result<(), Error> {
     match response {
         Ok(data) => {
             let items: Vec<Item> = data.json::<Items>().await?.results;
-            
+
+            let mut v : Vec<String> = Vec::new();
+
             for item in items {
-                if item.original_name.is_some() {
-                    let print_date = if item.first_air_date.as_ref().unwrap().is_empty() { "unknown".to_string().blue() } else { item.first_air_date.unwrap().blue() };
-                    let original_language = if item.original_language.as_ref().unwrap().is_empty() { "unknown".to_string().blue() } else { item.original_language.unwrap().blue() };
-                    
-                    let item_title = "title:".to_string().grey();
-                    let item_released = "released:".to_string().grey();
-                    let item_language = "language:".to_string().grey();
-                    let item_id = "tmdb_id:".to_string().grey();
+                let found_title = if item.original_name.as_ref().is_some() { item.original_name } else { item.original_title };
+                let found_release_date = if item.first_air_date.as_ref().is_some() { item.first_air_date } else { item.release_date };
 
-                    // This can probably improve quite a lot.
-                    println!("{} {} - {} {} - {} {} - {} {}", item_title, &item.original_name.unwrap().blue(), item_released, print_date, item_language, original_language, item_id, &item.id.to_string().blue())
-                } else {
-                    let print_date = if item.release_date.as_ref().unwrap().is_empty() { "unknown".to_string().blue() } else { item.release_date.unwrap().blue() };
-                    let original_language = if item.original_language.as_ref().unwrap().is_empty() { "unknown".to_string().blue() } else { item.original_language.unwrap().blue() };
-                    
-                    let item_title = "title:".to_string().grey();
-                    let item_released = "released:".to_string().grey();
-                    let item_language = "language:".to_string().grey();
-                    let item_id = "tmdb_id:".to_string().grey();
+                let strr = format!("{} - {} - {} - {} - {}", item.media_type.unwrap(), found_title.unwrap(), found_release_date.unwrap(), item.original_language.unwrap(), item.id.to_string());
 
-                    // This can probably improve quite a lot.
-                    println!("{} {} - {} {} - {} {} - {} {}", item_title, &item.original_title.unwrap().blue(), item_released, print_date, item_language, original_language, item_id, &item.id.to_string().blue())
-                }
+                v.push(strr);
             }
-        },
+
+            let index = Select::with_theme(&ColorfulTheme::default())
+                .with_prompt("Pick an Activity:")
+                .default(0)
+                .items(&v)
+                .interact();
+
+            let selected_index = &index.unwrap();
+            let selected_value = &v[*selected_index];
+            let selected_values: Vec<&str> = selected_value.split('-').collect();
+            let tmdb_id = selected_values[6].replace(" ", "");
+
+            println!("You selected {:?}", tmdb_id);
+        }
         Err(_error) => {
             println!("Error sending request, server could not be reached?");
-        },
+        }
     }
 
     Ok(())
